aa4ec2eb9f895d65aa460720b113bb204a275511
warlleism <warllei.s.martins@hotmail.com>
2025-07-21 12:26:23 -0300

    correções

--- app/hooks/api.ts
+++ app/hooks/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    baseURL: "http://189.50.3.3:3308",
+    // baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 export default api;
