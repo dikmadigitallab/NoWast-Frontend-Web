3841de8985054ccd38fb1920dc3a71a387db836b
Oscar <oscardikma@gmail.com>
2025-09-01 17:39:46 -0300

    test

--- app/hooks/usuarios/login.tsx
+++ app/hooks/usuarios/login.tsx
@@ -20,7 +20,7 @@ export const useLogin = () => {
 
         try {
             const clearFormatedData = data?.replace(/[.\-]/g, '')
-            const response = await axios.post('https://nowastev2.api.dikmadigital.com.br/auth', { document: clearFormatedData, password,Credentials: 'include' });
+            const response = await axios.post('dhttps://nowastev2.api.dikmadigital.com.br/auth', { document: clearFormatedData, password,Credentials: 'include' });
             document.cookie = `authToken=${response.data.data.token}; Path=/; Max-Age=3600; SameSite=Lax`;
 
             if (response.data.data.user.role.name === "Administrador Dikma") {
@@ -52,7 +52,7 @@ export const useLogin = () => {
             }, 1000);
 
         } catch (error) {
-            toast.error("Documento ou senha inválidos! " + error);
+            toast.error("Documento ou senha inválidos!");
             setError((error as any)?.response?.data?.message || null);
         } finally {
             setIsLoading(false);
