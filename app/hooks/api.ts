1ae7a8678d0a1b63029a9586e203e9c0cf46303f
Oscar <oscardikma@gmail.com>
2025-09-04 10:54:52 -0300

    update api

--- app/hooks/api.ts
+++ app/hooks/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://nowastev2.api.dikmadigital.com.br/ '
+    baseURL: 'https://nowastev2.api.dikmadigital.com.br/'
     //process.env.NEXT_PUBLIC_API_URL
 });
 
